0085708dcfb55248c99694417462781429cf4a37
Yixi Chen <ecchen96153@gmail.com>
2017-06-21 16:21:13 -0500

    add project images

--- animation.js
+++ animation.js
@@ -1,5 +1,5 @@
 $(function() {
-	$('a').on('click', function(e) {
+	$('.nav-button').on('click', function(e) {
 		e.preventDefault();
 		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 	});
