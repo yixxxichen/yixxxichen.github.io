5bc358b3d035d04caef7df7bcb3b99bd776eddf3
Yixi Chen <ecchen96153@gmail.com>
2017-11-14 23:58:42 -0600

    add a personal code file

--- animation.js
+++ animation.js
@@ -1,5 +1,5 @@
 $(function() {
-	$('.nav-button').on('click', function(e) {
+	$('.nav-button,#homedownbutton').on('click', function(e) {
 		e.preventDefault();
 		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 	});
